1c7f146287cc2ef70d2438453b8257c79b372b72
Ansible automation codes <144895375+ansible-codes@users.noreply.github.com>
2024-06-11 15:01:02 +0100

    Update io_stats.sh

--- io_stats.sh
+++ io_stats.sh
@@ -1,3 +1,2 @@
-iostat -c 1 | awk '/avg-cpu/ {getline; printf "\r%%iowait: %.2f%%", $4; fflush()}'
-
-iostat -c 1 | awk '/avg-cpu/ {getline; print strftime("%Y-%m-%d %H:%M:%S"), "%iowait: " $4"%"}' >> iowait_log.txt
+iostat -c 1 2 | awk '/avg-cpu/ {getline; printf "%s %%iowait: %.2f%%\n", strftime("%Y-%m-%d %H:%M:%S"), $4}'
+iostat -c 1 2 | awk '/avg-cpu/ {getline; print "%iowait: " $4"%"}'
